552a1b4374b3d14e3edf47572f923a94b841a9a8
Thiago <thiagolemos@id.uff.br>
2024-04-06 16:04:13 -0300

    Adicionar comentário com todas as operações da regex

--- geradordescanner.py
+++ geradordescanner.py
@@ -1,4 +1,27 @@
 # *(.(+(a,b),+(a,E)))
+""" 
+Concatenação: A sequência simples de caracteres um após o outro representa a concatenação. Por exemplo, a expressão regular ab corresponde à sequência "ab".
+
+União ou alternância (|): Representa a escolha entre dois ou mais padrões. Por exemplo, a|b corresponde a "a" ou "b".
+
+Fecho de Kleene (*): Indica que o símbolo anterior pode ser repetido zero ou mais vezes. Por exemplo, a* corresponde a "", "a", "aa", "aaa", etc.
+
+Fecho positivo (+): Similar ao fecho de Kleene, mas o símbolo anterior deve aparecer pelo menos uma vez. Por exemplo, a+ corresponde a "a", "aa", "aaa", etc., mas não a "".
+
+Opcional (?): Indica que o símbolo anterior pode aparecer zero ou uma vez. Por exemplo, a? corresponde a "" ou "a".
+
+Conjuntos de caracteres ([ ]): Especificam um conjunto de caracteres dos quais qualquer um pode ser aceito. Por exemplo, [abc] corresponde a "a", "b", ou "c". Intervalos podem ser especificados usando um hífen, como em [a-z] para qualquer letra minúscula.
+
+Negação (^): Quando usado dentro de um conjunto de caracteres, nega o conjunto. Por exemplo, [^a] corresponde a qualquer caractere exceto "a".
+
+Grupos e subpadrões ( ): Agrupam partes de uma expressão regular, permitindo que você aplique operadores como * ou + a um grupo inteiro. Por exemplo, (ab)+ corresponde a "ab", "abab", "ababab", etc.
+
+Ancoragem:
+
+Início de linha (^): Quando usado fora de um conjunto de caracteres, corresponde ao início de uma linha.
+Fim de linha ($): Corresponde ao final de uma linha.
+Por exemplo, ^a corresponde a qualquer "a" no início de uma linha, e a$ corresponde a "a" no final de uma linha.
+Escape (\): Usado para escapar caracteres especiais, permitindo que sejam tratados como caracteres comuns. Por exemplo, \\. corresponde a um ponto literal, em vez de qualquer caractere."""
  
 class State:
     def __init__(self, is_final= False):
