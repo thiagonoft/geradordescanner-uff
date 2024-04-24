87645e4885aa7a8869a7f2a0d217a3a516888302
Gustavo Queiroz <gustavohqueiroz22@gmail.com>
2024-04-24 16:33:39 -0300

    codigo exemplo em basic e regex python (com erro)

---
+++ teste.py
@@ -0,0 +1,24 @@
+import re
+
+PROGRAM_INPUT = "int x = 10;"
+
+KEYWORDS = [
+    "int",
+    "char"
+]
+
+DELIMITERS = [
+    ";"
+]
+
+OPERATORS = [
+    "="
+]
+
+IDENTIFIER_REGEX = r"([A-z]+)|[A-z](([A-z]|[0-9])*)"
+
+CONSTANT_REGEX = r"[0-9]+"
+# ????? nao sei pq n deu
+if __name__ == "__main__":
+    match = re.findall(IDENTIFIER_REGEX, PROGRAM_INPUT)
+    print(match)
