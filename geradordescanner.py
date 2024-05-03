423fbb242da37621221e1b6e6c357bbe93118b27
Thiago Almeida <thiagolemos@id.uff.br>
2024-05-03 11:54:34 -0300

    Considera expressoes do tipo ([A-z])* ou +

--- geradordescanner.py
+++ geradordescanner.py
@@ -1,61 +1,63 @@
 # *(.(+(a,b),+(a,E)))
 """
 Concatenação: A sequência simples de caracteres um após o outro representa a concatenação. Por exemplo, a expressão regular ab corresponde à sequência "ab".
- 
+
 União ou alternância (|): Representa a escolha entre dois ou mais padrões. Por exemplo, a|b corresponde a "a" ou "b".
- 
+
 Fecho de Kleene (*): Indica que o símbolo anterior pode ser repetido zero ou mais vezes. Por exemplo, a* corresponde a "", "a", "aa", "aaa", etc.
- 
+
 Fecho positivo (+): Similar ao fecho de Kleene, mas o símbolo anterior deve aparecer pelo menos uma vez. Por exemplo, a+ corresponde a "a", "aa", "aaa", etc., mas não a "".
- 
+
 Opcional (?): Indica que o símbolo anterior pode aparecer zero ou uma vez. Por exemplo, a? corresponde a "" ou "a".
- 
+
 Conjuntos de caracteres ([ ]): Especificam um conjunto de caracteres dos quais qualquer um pode ser aceito. Por exemplo, [abc] corresponde a "a", "b", ou "c". Intervalos podem ser especificados usando um hífen, como em [a-z] para qualquer letra minúscula.
- 
+
 Negação (^): Quando usado dentro de um conjunto de caracteres, nega o conjunto. Por exemplo, [^a] corresponde a qualquer caractere exceto "a".
- 
+
 Grupos e subpadrões ( ): Agrupam partes de uma expressão regular, permitindo que você aplique operadores como * ou + a um grupo inteiro. Por exemplo, (ab)+ corresponde a "ab", "abab", "ababab", etc.
- 
+
 Ancoragem:
- 
+
 Início de linha (^): Quando usado fora de um conjunto de caracteres, corresponde ao início de uma linha.
 Fim de linha ($): Corresponde ao final de uma linha.
 Por exemplo, ^a corresponde a qualquer "a" no início de uma linha, e a$ corresponde a "a" no final de uma linha.
 Escape (\): Usado para escapar caracteres especiais, permitindo que sejam tratados como caracteres comuns. Por exemplo, \\. corresponde a um ponto literal, em vez de qualquer caractere."""
- 
+
 SPECIAL_CHARS = "+,)(.*"
+nfas = []
+
 class State:
     def __init__(self, is_final=False):
         self.is_final = is_final
         self.transitions = {}
         self.episilon_transitions = []
- 
+
     def add_transition(self, symbol, next_state):
         if symbol == 'ε':
             self.episilon_transitions.append(next_state)
- 
+
         if symbol not in self.transitions:
             self.transitions[symbol] = []
         self.transitions[symbol].append(next_state)
- 
+
     def add_range_transitions(self, start_char, end_char, state):
         for ascii_code in range(ord(start_char), ord(end_char) + 1):
             self.add_transition(chr(ascii_code), state)
- 
- 
+
+
 class NFA:
     def __init__(self, start_state):
         self.start_state = start_state
         self.states = [start_state]
- 
+
     def add_state(self, state):
         self.states.append(state)
 
     def merge_nfa(self, other_nfa):
         for state in other_nfa.states:
             self.add_state(state)
- 
- 
+
+
 def pop_until_open_parenthesis(exp_stack):
     """
     Desempilha caracteres até encontrar o parêntese aberto correspondente,
@@ -63,7 +65,7 @@ def pop_until_open_parenthesis(exp_stack):
     """
     nested = 0  # Para controlar parênteses aninhados
     expr = []
- 
+
     while exp_stack:
         char = exp_stack.pop()
         if char == '(':
@@ -73,125 +75,142 @@ def pop_until_open_parenthesis(exp_stack):
         elif char == ')':
             nested += 1
         expr.append(char)
- 
+
     # Retorna a expressão desempilhada e invertida, pois a desempilhamos em ordem reversa
     return ''.join(reversed(expr))
- 
- 
+
+
 def process_plus_operator(exp_stack):
     # Processa o operador '+'
     expression = pop_until_open_parenthesis(exp_stack)
     expression_elements = [c for c in expression if c not in SPECIAL_CHARS]
 
-    # Cria o estado inicial do NFA
-    initial_state = State()
-    nfa = NFA(initial_state)
-    current_state = initial_state
+    if 'NFA_' in expression:
+        position = expression.find('_')
+        nfa = nfas[int(expression[position+1:position+2])]
+        final_state = [state for state in nfa.states if state.is_final]
+        final_state[0].add_transition('ε', nfa.start_state)
+        nfas.pop(int(expression[position+1:position+2]))
+    else:
+        # Cria o estado inicial do NFA
+        initial_state = State()
+        nfa = NFA(initial_state)
+        current_state = initial_state
 
-    # Cria estados e transições para cada elemento na expressão
-    for i, element in enumerate(expression_elements):
-        next_state = State()
-        current_state.add_transition(element, next_state)
-        nfa.add_state(next_state)
-        current_state = next_state
+        # Cria estados e transições para cada elemento na expressão
+        for i, element in enumerate(expression_elements):
+            next_state = State()
+            current_state.add_transition(element, next_state)
+            nfa.add_state(next_state)
+            current_state = next_state
 
-    # Faz o último estado apontar para o primeiro e marca como final
-    current_state.add_transition('ε', initial_state)
-    current_state.is_final = True
- 
+        # Faz o último estado apontar para o primeiro e marca como final
+        current_state.add_transition('ε', initial_state)
+        current_state.is_final = True
+
     return nfa
- 
- 
+
+
 def process_concatenation(exp_stack):
     # Processa a concatenação
     expression = pop_until_open_parenthesis(exp_stack)
     expression_elements = [c for c in expression if c not in SPECIAL_CHARS]
- 
+
     S1 = State()
     S2 = State()
     S3 = State(is_final=True)
- 
+
     S1.add_transition(expression_elements[0], S2)
     S2.add_transition(expression_elements[1], S3)
- 
+
     nfa = NFA(S1)
     nfa.add_state(S1)
     nfa.add_state(S2)
     nfa.add_state(S3)
- 
+
     return nfa
- 
- 
+
+
 def process_union(exp_stack):
     # Processa a união
     expression = pop_until_open_parenthesis(exp_stack)
     expression_elements = [c for c in expression if c not in SPECIAL_CHARS]
- 
+
     S1 = State()
     S2 = State()
     S3 = State()
     S4 = State(is_final=True)
- 
+
     S1.add_transition('ε', S2)
     S1.add_transition('ε', S3)
     S2.add_transition(expression_elements[0], S4)
     S3.add_transition(expression_elements[1], S4)
- 
+
     nfa = NFA(S1)
     nfa.add_state(S1)
     nfa.add_state(S2)
     nfa.add_state(S3)
     nfa.add_state(S4)
- 
+
     return nfa
- 
- 
+
+
 def process_kleene_closure(exp_stack):
     # Processa o fecho de Kleene
     expression = pop_until_open_parenthesis(exp_stack)
     expression_elements = [c for c in expression if c not in SPECIAL_CHARS]
 
-    # Cria um estado inicial que também é o estado final (aceitação)
-    initial_state = State(is_final=True)
-    current_state = initial_state
+    if 'NFA_' in expression:
+        position = expression.find('_')
+        nfa = nfas[int(expression[position+1:position + 2])]
+        final_state = [state for state in nfa.states if state.is_final]
+        final_state[0].add_transition('ε', nfa.start_state)
+        nfa.start_state.is_final = True
+        nfas.pop(int(expression[position + 1:position + 2]))
+    else:
+        # Cria um estado inicial que também é o estado final (aceitação)
+        initial_state = State(is_final=True)
+        current_state = initial_state
 
-    # Cria um NFA com o estado inicial
-    nfa = NFA(initial_state)
+        # Cria um NFA com o estado inicial
+        nfa = NFA(initial_state)
 
-    # Loop para adicionar estados e transições para cada elemento na expressão
-    for element in expression_elements:
-        next_state = State()
-        current_state.add_transition(element, next_state)
-        nfa.add_state(next_state)
-        current_state = next_state
+        # Loop para adicionar estados e transições para cada elemento na expressão
+        for element in expression_elements:
+            next_state = State()
+            current_state.add_transition(element, next_state)
+            nfa.add_state(next_state)
+            current_state = next_state
 
-    # Adiciona transição epsilon do último estado para o estado inicial
-    current_state.add_transition('ε', initial_state)
+        # Adiciona transição epsilon do último estado para o estado inicial
+        current_state.add_transition('ε', initial_state)
 
     return nfa
- 
+
+
 def process_optional(exp_stack):
     # Processa o símbolo opcional
     expression = pop_until_open_parenthesis(exp_stack)
     expression_elements = [c for c in expression if c not in SPECIAL_CHARS]
- 
+
     S1 = State()
     S2 = State(is_final=True)
- 
+
     S1.add_transition('ε', S2)
     S1.add_transition(expression_elements[0], S2)
- 
+
     nfa = NFA(S1)
     nfa.add_state(S1)
     nfa.add_state(S2)
- 
+
     return nfa
+
 
 def process_character_set(expression, exp_stack):
     # Assume que a expressão vem no formato [a-z] ou [abc]
     # Primeiro, removemos os colchetes
     content = expression[1:-1]
-    
+
     start_state = State()
     final_state = State(is_final=True)
 
@@ -206,7 +225,8 @@ def process_character_set(expression, exp_stack):
     nfa.add_state(start_state)
     nfa.add_state(final_state)
     return nfa
- 
+
+
 def print_nfa(nfa):
     # Cria um mapeamento completo de todos os estados acessíveis a partir do estado inicial
     state_index = {}
@@ -234,6 +254,8 @@ def print_nfa(nfa):
     for state in state_index:
         state_idx = state_index[state]
         print(f'Transições do estado {state_idx}:')
+        if state.is_final:
+            print(f'Este estado é FINAL')
         for symbol, transitions in state.transitions.items():
             for transition in transitions:
                 transition_idx = state_index[transition]
@@ -241,6 +263,7 @@ def print_nfa(nfa):
         for transition in state.episilon_transitions:
             transition_idx = state_index[transition]
             print(f"  Símbolo: ε, Vai para: Estado {transition_idx}")
+
 
 def add_epsilon_transitions_between_nfas(nfas):
     combined_nfa = nfas[0]  # Começa com o primeiro NFA
@@ -262,12 +285,12 @@ def add_epsilon_transitions_between_nfas(nfas):
 
     return combined_nfa
 
+
 def scan_expression(exp: str):
     exp_splitted = list(exp)
     exp_stack = []
-    nfas = []
-    skip_until_index = -1 # Para controlar a captura de conjuntos de caracteres
- 
+    skip_until_index = -1  # Para controlar a captura de conjuntos de caracteres
+
     for i, c in enumerate(exp_splitted):
         print(exp_stack)
         if i < skip_until_index:
@@ -279,7 +302,7 @@ def scan_expression(exp: str):
             while exp_splitted[i] != ']':
                 i += 1
             # Captura o conteúdo entre os colchetes, inclusive
-            char_set = ''.join(exp_splitted[start_index:i+1])
+            char_set = ''.join(exp_splitted[start_index:i + 1])
             nfa = process_character_set(char_set, exp_stack)  # Processa o conjunto de caracteres
             exp_stack.append(f"NFA_{len(nfas)}")
             nfas.append(nfa)
@@ -306,16 +329,14 @@ def scan_expression(exp: str):
             nfas.append(nfa)
         else:
             exp_stack.append(c)
- 
+
     nfa_final = add_epsilon_transitions_between_nfas(nfas)
     # print(exp_splitted)
     print(exp_stack)
 
-
     print_nfa(nfa_final)
 
-
     return exp
- 
- 
-scan_expression("(ab)+(7)*(@)+")
+
+
+scan_expression("(a,T)*")
