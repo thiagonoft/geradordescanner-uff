e602f01f7ee0d26fa22ea51a2f8bea4ecf327c92
Gustavo Queiroz <gustavohqueiroz22@gmail.com>
2024-05-24 11:17:28 -0300

    fazendo backtracking

--- parser/parser_v2.py
+++ parser/parser_v2.py
@@ -8,11 +8,16 @@ class DecisionPoint:
         curr_tokens: list,
         # curr_pt_bools: dict,
         marked_line: str,
-        marked_col: str):
+        marked_col: str,
+        numOfRulesToApply: int,
+        DEBUG_RULE_TO_APPLY):
     self.curr_stack = curr_stack.copy()
     self.curr_tokens = curr_tokens.copy()
     self.marked_line = marked_line
     self.marked_col = marked_col
+    self.already_tried = [False]*numOfRulesToApply
+    self.DEBUG_RULE_TO_APPLY = DEBUG_RULE_TO_APPLY
+    
     # mudar pra 'already_tried'
     # self.curr_pt_bools = copy.deepcopy(curr_pt_bools)
     
@@ -49,16 +54,72 @@ def initParsingTableCopyWithBooleans(pt):
     
     return result_pt
 
-def decideIndexOfRuleToApply(line, col, pt_bools):
-    for i, isMarked in enumerate(pt_bools[line][col]):
-        if not isMarked:
-            # SÓ PRA TESTE
-            return len(pt_bools[line][col]) - i - 1
-            # return i
-    raise Exception("Todas as regras possiveis foram marcadas!!")
+def decideIndexOfRuleToApply(line, col):#, pt_bools):
+    # if DEBUG_IS_INDEX_REVERSED:
+    #     for i, isMarked in enumerate(reversed(pt_bools[line][col])):
+    #         if not isMarked:
+    #             return len(pt_bools[line][col]) - i - 1
+    # else:
+    #     for i, isMarked in enumerate(pt_bools[line][col]):
+    #         if not isMarked:
+    #             return i
+    if len(decision_points) == 0:
+        return len(PARSING_TABLE[line][col]) - 1
+    
+    lp = decision_points[-1]
+    just_backtracked = (stack == lp.curr_stack) and (tokens == lp.curr_tokens)
+    if just_backtracked:
+        if DEBUG_IS_INDEX_REVERSED:
+            i = getLastFalseIndex(lp.already_tried)
+            return i
+        else:
+            return len(PARSING_TABLE[line][col]) - 1
+    else:
+        return len(PARSING_TABLE[line][col]) - 1
+    # raise Exception("Todas as regras possiveis foram marcadas!!")
 
 with open('parsing_table.json') as json_file:
     PARSING_TABLE = json.load(json_file)
+
+def markDecisionPoint(dp: DecisionPoint):
+    marked = False
+    if DEBUG_IS_INDEX_REVERSED:
+        for i, isMarked in enumerate(reversed(dp.already_tried)):
+            if not isMarked:
+                dp.already_tried[len(dp.already_tried) - i - 1] = True
+                marked = True
+                break
+    else:
+        for i, isMarked in enumerate(dp.already_tried):
+            if not isMarked:
+                dp.already_tried[i] = True
+                marked = True
+                break
+    return marked 
+    # if not marked:
+    #     raise Exception("Falha ao marcar DecisionPoint!! Todas as possibilidades foram marcadas!!")
+
+def backtrack():
+    last_point: DecisionPoint = decision_points[-1]
+    print(f"Backtracking on decision {last_point.DEBUG_RULE_TO_APPLY}...")
+    l = last_point.marked_line
+    c = last_point.marked_col
+    prev = PARSING_TABLE[l][c]
+    markDecisionPoint(last_point)
+    #TODO: tratar qnd tiver 0 possibilidades (regra nao é mais vivel)
+    print("Remaining possibilities:")
+    for i, isMarked in enumerate(last_point.already_tried):
+        if not isMarked:
+            print("    ", prev[i])
+    global tokens
+    tokens = last_point.curr_tokens
+    global stack
+    stack = last_point.curr_stack
+
+def getLastFalseIndex(l: list[bool]):
+    for i, e in enumerate(reversed(l)):
+        if e == False:
+            return len(l) - i - 1
 
 tokens = []
 # with open('tokens_input.txt', "r") as file:
@@ -97,30 +158,16 @@ tokens.append("($, $)")
 
 
 
-
+# TODO: tirar isso e receber direto do gerador de scanner
 TYPE_MAPPING_DICT = {
     "NUMBER": "Integer",
     "RELATIONAL_OPERATOR": "=",
 }
 
-# ADHOC_DECISION_MAPPING_DICT = {
-#     "<Lines>Integer":"<Lines> -> Integer <Statements> NewLine",
-#     "<Statements>IF":"<Statements> -> <Statement>",
-#     "<Expression>ID":"<Expression> -> <AndExp>",
-#     "<AndExp>ID":"<AndExp> -> <NotExp>",
-#     "<CompareExp>ID":"<CompareExp> -> <AddExp> = <CompareExp>",
-#     "<AddExp>ID":"<AddExp> -> <MultExp>",
-#     "<MultExp>ID":"<MultExp> -> <NegateExp>",
-#     "<PowerExp>ID":"<PowerExp> -> <Value>",
-#     "<Value>ID":"<Value> -> ID",
-#     "<CompareExp>Integer":"<CompareExp> -> <AddExp>",
-#     "<AddExp>Integer":"<AddExp> -> <MultExp>",
-#     "<MultExp>Integer":"<MultExp> -> <NegateExp>",
-#     "<PowerExp>Integer":"<PowerExp> -> <Value>"
-# }
+DEBUG_IS_INDEX_REVERSED = True
 
-decision_points = []
-pt_bools = initParsingTableCopyWithBooleans(PARSING_TABLE)
+decision_points: list[DecisionPoint] = []
+# pt_bools = initParsingTableCopyWithBooleans(PARSING_TABLE)
 
 stack = ['$', "<Lines>"]
 stack_top = stack[-1]
@@ -130,20 +177,25 @@ curr_token = convertToken(curr_token_annotated)
 while len(stack) > 0:
     if isNonTerminal(stack_top):
         if not curr_token in PARSING_TABLE[stack_top]:
-            raise Exception("COLUNA vazia na tabela de parsing ! Erro de sintaxe!!!")
             #TODO: backtrack aqui (para o caso de teste atual, a primeira regra <CompareExp> não
             # pode virar '<AddExp>' e sim tem que virar '<AddExp> = <CompareExp>'
+            backtrack()
+            continue
+            # break
+            # raise Exception("COLUNA vazia na tabela de parsing ! Erro de sintaxe!!!")
+        
         rules_to_apply = PARSING_TABLE[stack_top][curr_token]
         if len(rules_to_apply) > 1:
             line = str(stack_top)
             col = str(curr_token)
 
-            new_dp = DecisionPoint(stack, tokens, line, col)
-            decision_points.append(new_dp)
-            
-            index = decideIndexOfRuleToApply(line, col, pt_bools)
+            # TODO: revisar a função 'decideIndexOfRuleToApply'
+            index = decideIndexOfRuleToApply(line, col)
             rule_to_apply = PARSING_TABLE[line][col][index]
             print(rule_to_apply)
+            
+            new_dp = DecisionPoint(stack, tokens, line, col, len(rules_to_apply), rule_to_apply)
+            decision_points.append(new_dp)
             # rule_to_apply = ADHOC_DECISION_MAPPING_DICT[line + col]
         elif len(rules_to_apply) == 1:
             rule_to_apply = rules_to_apply[0]
